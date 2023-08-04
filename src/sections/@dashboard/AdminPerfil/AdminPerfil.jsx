709a21872aa60b10dc717449369ecaf50a3dce46
zerontec <zerontecnologia1@gmail.com>
2023-08-03 22:37:52 -0400

    fix component delete modal

--- src/sections/@dashboard/AdminPerfil/AdminPerfil.jsx
+++ src/sections/@dashboard/AdminPerfil/AdminPerfil.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/button-has-type */
 /* eslint-disable no-unused-expressions */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
@@ -23,12 +24,12 @@ import {
   Typography,
   TextField,
   Button,
-  Alert
+  Alert,
 } from '@mui/material';
 import Swal from 'sweetalert2';
 import { DriveFolderUpload } from '@mui/icons-material';
 import { serachUsereById } from '../../../redux/modules/user';
-import { createPayment, deleteUpload, getAllLoans, updateLoand } from '../../../redux/modules/loan';
+import {  deleteUpload, getAllLoans,  updateLoand } from '../../../redux/modules/loan';
 
 import { fDate, fDateTime } from '../../../utils/formatTime';
 import { FloatingButtonComponent } from '../../../components/FloatingButtonComponent';
@@ -109,27 +110,27 @@ const AdminPerfil = () => {
   const [rowsPerPage, setRowsPerPage] = useState(5);
   const [searchTerm, setSearchTerm] = useState('');
   const [selectedLoanId, setSelectedLoanId] = useState(null);
+
   const [open, setOpen] = useState(false);
   const [user, setUser] = useState(null);
   const [deudas, setDeudas] = useState([]);
+  const [abonos, setAbonos] = useState([]);
+
   const [errors, setErrors] = useState({});
   const [loading, setLoading] = useState(false);
   const [selectButton, setSelectButton] = useState(null);
-	const [messageError, setMessageError] = useState({});
+  const [messageError, setMessageError] = useState({});
   const { message } = useSelector((state) => state);
   const [selectedAbonoEdit, setSelectedAbonoEdit] = useState({
-    loanId:'',
+    loanId: '',
     amount: '',
     paymentDate: '',
-    
   });
-  
-  
-
 
   const [openEditModal, setOpenEditModal] = useState(false);
+
   const [openAbonoModal, setOpenAbonoModal] = useState(false);
-  
+
   const handleOpenEditModal = (loan) => {
     setSelectedLoanId(loan.id);
     setSelectedLoanEdit({
@@ -139,50 +140,22 @@ const AdminPerfil = () => {
     });
     setOpenEditModal(true);
   };
-  
+
   const handleOpenAbonoModal = (loanId) => {
     setSelectedLoanId(loanId);
-    setFormInfo((prevFormInfo) => ({
-      ...prevFormInfo,
-      loanId,
-    }));
     setOpenAbonoModal(true);
   };
-  
+
   const handleCloseEditModal = () => {
     setOpenEditModal(false);
   };
-  
+
   const handleCloseAbonoModal = () => {
     setOpenAbonoModal(false);
   };
-  
 
+  // Clientes
 
-
-
-
-
-
-  const handleEditClick = (loan) => {
-    setSelectedLoanId(loan.id);
-    setSelectedLoanEdit({
-      amount: loan.amount,
-      notes: loan.notes,
-      status: loan.status,
-    });
-    setOpen(true);
-  };
-
-  const handleAbonoClick = (loan) => {
-    setSelectedLoanId(loan.id);
-    setSelectedAbonoEdit({
-      loanId:'',
-      amount: '',
-      paymentDate: ''
-    });
-    setOpen(true);
-  };
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -198,21 +171,14 @@ const AdminPerfil = () => {
     setOpen(false);
   };
 
-  const handleCloseModalA = () => {
-    setSelectedLoanId(null);
-    setSelectedAbonoEdit({
-      loanId:'',
-      amount: '',
-      paymentDate: ''
-    });
-    setOpen(false);
-  };
+
 
   const [selectedLoanEdit, setSelectedLoanEdit] = useState({
     amount: '',
     nota: '',
     status: '',
   });
+
   const handleSubmitEdit = (e) => {
     if (selectedLoanEdit.amount && selectedLoanEdit.notes) {
       e.preventDefault();
@@ -223,10 +189,10 @@ const AdminPerfil = () => {
       };
       dispatch(updateLoand(selectedLoanId, data));
       Swal.fire('¨Prestamo Editado con Exito  !', 'You clicked the button!', 'success');
-      window.location.reload();
+    
       dispatch(getAllLoans());
 
-      handleCloseModal();
+      handleCloseEditModal();
       //   getAllAnalysis();
     } else {
       Swal.fire({
@@ -239,9 +205,6 @@ const AdminPerfil = () => {
     }
   };
 
-
-
-
   const handleChangeRowsPerPage = (event) => {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
@@ -250,23 +213,23 @@ const AdminPerfil = () => {
   const usuarios = useSelector((state) => state.usuarios);
   const deuda = useSelector((state) => state.loan);
 
-
   console.log('usuarios', usuarios);
   console.log('deuda', deuda);
-  
+
+
+
   useEffect(() => {
-    // Llamada a la API para obtener los datos del cliente
     dispatch(serachUsereById({ id }))
       .then((response) => setUser(response))
       .catch((error) => console.log(error));
-    // dispatch(getClientPurchase({id}))
-    // .then((response) => setCompras(response))
-    // .catch((error) => console.log(error));
 
     dispatch(getAllLoans())
-      .then((response) => setDeudas(response))
-      .catch((error) => console.log(error));
+      // .then((response) => setDeudas(response))
+      // .catch((error) => console.log(error));
   }, [id, dispatch]);
+
+
+
 
   function deleteHandler(item) {
     Swal.fire({
@@ -281,10 +244,7 @@ const AdminPerfil = () => {
       if (result.isConfirmed) {
         dispatch(deleteUpload(item.id));
         Swal.fire('la deuda  ha sido borrado!');
-
-        setTimeout(() => {
-          window.location.reload();
-        }, 500);
+        dispatch(getAllLoans());
       } else {
         Swal.fire('la cuenta  Esta Seguro !');
       }
@@ -293,95 +253,10 @@ const AdminPerfil = () => {
 
   fDateTime();
 
-
   const [formInfo, setFormInfo] = useState({
-	 
-	
-	
-	  amount: "",
-	  paymentDate: "",
-	});
-	const [isFormValid, setIsFormValid] = useState(false);
-
-	const validateForm = () => {
-		const { amount,paymentDate } = formInfo;
-		setIsFormValid(
-		 
-		  amount.trim() !== "" &&
-		 paymentDate.trim() !== "" 
-		 
-		);
-	  };
-
-	  useEffect(() => {
-      validateForm();
-      }, [formInfo]);
-  
-      function validate(formInfo) {
-	
-        const errors = {};
-        
-        formInfo.amount
-        ? (errors.amount = "")
-        : (errors.amount = "Ingrese Nombre de Producto");
-        formInfo.paymentDate
-        ? (errors.paymentDate = "")
-        : (errors.paymentDate = "Ingrese una Descripcion");
-      
-       
-        return errors;
-      }
-
-      const handleChange = (event) => {
-        const { name, value } = event.target;
-        setFormInfo((prevFormInfo) => ({
-        ...prevFormInfo,
-        [name]: value,
-        }));
-        setErrors(validate({ ...formInfo, [name]: value }));
-      };
-
-     
-      
-      // Crear Abono
-
-   const handleSubmitAbono = (event) => {
-	  event.preventDefault();
-    const data = {
-      ...formInfo,
-      loanId: selectedLoanId, // Agrega el ID de la deuda al objeto data
-    };
-	  setLoading(true);
-	  dispatch(createPayment(data))
-		.then((response) => {
-		  setLoading(false);
-   
-		  Swal.fire("Abono creado con éxito!", "", "success");
-		   window.location.reload();
-		  setFormInfo({
-		amount:'',
-    paymentDate:''
-		  });
-		  setSelectButton(null);
-      handleCloseAbonoModal()
-		  if (response.error) {
-			setMessageError(response.error);
-		  }
-		})
-		.catch((error) => {
-		  console.log(error);
-		  setLoading(false);
-		  setSelectButton(null);
-		  setMessageError(error.message);
-		  Swal.fire(error.message);
-		});
-	};
-
-
-  // const handleOpenAbonoModal = (loanId) => {
-  //   setSelectedLoanId(loanId);
-  //   setOpen('abono');
-  // };
+    amount: '',
+    paymentDate: '',
+  });
 
 
   return (
@@ -467,104 +342,13 @@ const AdminPerfil = () => {
             </form>
           )}
           <hr />
-          <Button variant="contained" onClick={() => setOpen(null)}>
+          <Button variant="contained" onClick={() => handleCloseEditModal()}>
             Cerrar
           </Button>
         </Box>
       </Modal>
 
-
-
-
-
-
-
-{/* Modal Abono  */}
-
-
-<Modal open={openAbonoModal} onClose={handleCloseAbonoModal}>
-		  <Box
-			sx={{
-			  position: "absolute",
-			  top: "50%",
-			  left: "50%",
-			  transform: "translate(-50%, -50%)",
-			  width: 400,
-			  bgcolor: "background.paper",
-			  borderRadius: "8px",
-			  boxShadow: 24,
-			  p: 4,
-			}}
-		  >
-			{/* Aquí va el contenido del modal */}
-			<form onSubmit={handleSubmitAbono}>
-			  <FormContainer>
-				<FieldContainer>
-        <TextField
-  required
-  label="ID de la deuda"
-  name="loanId"
-  type="text"
-  id="loanId"
-  value={selectedLoanId || ""}
-  disabled
-/>{" "}
-				  {errors.barcode && (
-					<span className="error-message"> {errors.barcode}</span>
-				  )}
-				  <TextField
-					required
-					label="Monto"
-					name="amount"
-					type="number"
-					id="amount"
-					value={formInfo.amount}
-					onChange={handleChange}
-				  />{" "}
-				  {errors.amount && (
-					<span className="error-message"> {errors.amount}</span>
-				  )}
-				  <TextField
-					required
-					label="fecha"
-					name="paymentDate"
-					id="paymentDate"
-          type='date'
-					value={formInfo.paymentDate}
-					onChange={handleChange}
-				  />{" "}
-				  {errors.paymentDate && (
-					<span className="error-message"> {errors.paymentDate}</span>
-				  )}
-				
-  
-				  {message && (
-					<Alert severity="error" sx={{ mt: 2 }}>
-					  {" "}
-					  {messageError}{" "}
-					</Alert>
-				  )}{" "}
-				</FieldContainer>
-				<ActionsContainer>
-				<Button
-  type="submit"
-  onClick={handleSubmitAbono}
-  variant="contained"
-  color="primary"
-  disabled={!isFormValid} // Deshabilitar el botón si isFormValid es false
->
-  {loading ? "Cargando..." : "Crear Abono"}
-				  </Button>
-				</ActionsContainer>
-			  </FormContainer>
-			</form>
-			<hr />
-			
-		  </Box>
-		</Modal>
-
-
-
+      {/* Modal Abono  */}
 
       <StyledContainer>
         {/* {customer && ( */}
@@ -588,17 +372,28 @@ const AdminPerfil = () => {
               <strong>Fecha de Registro: {fDateTime(usuarios?.usuarios?.createdAt)}</strong>
             </Typography>
             {/* <Typography variant="body1">
-     
+     <hr/>
      <strong>Total gastado: {compra.invoices. totalPurchases}</strong>
    </Typography> */}
             {/* Agrega más datos del cliente según tus necesidades */}
-            <CreateSeller/>
-            <CreateLoan />
-           
-           
             
-          </Box>
+            
+            <hr/>
+            <Box display="flex" justifyContent="space-between" marginRight={20} marginBottom={2}>
+       
+            <Link to="/dashboard/prestamos-cliente">
+        <Button variant="contained">IR Administracio Prestamos Cliente</Button>
+        </Link>
+     
+        <CreateSeller  />
+        
+        <CreateLoan />
+       
+      </Box>
 
+<hr/>
+          </Box>
+         
           <Typography variant="h5" component="h3">
             Deudas Empleados
           </Typography>
@@ -635,12 +430,12 @@ const AdminPerfil = () => {
                           </Button>
                         </TableCell>
                         <TableCell className="tableCell">
-                        <Button onClick={() => handleOpenAbonoModal(item.id)}>Realizar Abono</Button>
+                          <Button variant="contained"  onClick={() => handleOpenAbonoModal(item.id)}>Abonar</Button>
                         </TableCell>
                         <TableCell className="tableCell">
-                          <div className="deleteButton" id={item.id} onClick={() => deleteHandler(item)}>
-                            <Button>Borrar</Button>
-                          </div>
+                         
+                            <Button variant='contained'  style={{ backgroundColor: "red", color: '#fff' }}  id={item.id} onClick={() => deleteHandler(item)}>Borrar</Button>
+                          
                         </TableCell>
                         <Link to={`/dashboard/perfil-empleados/${item.seller?.id}`} style={{ textDecoration: 'none' }}>
                           <button>Ver perfil</button>
@@ -665,10 +460,11 @@ const AdminPerfil = () => {
             />
           </TableContainer>
 
-
-<PaymentTable/>
-
-
+          <PaymentTable
+            loanId={selectedLoanId}
+            openAbonoModal={openAbonoModal}
+            handleCloseAbonoModal={() => setOpenAbonoModal(false)}
+          />
 
           <FloatingButtonComponent />
         </>
