b80f0b562a3aca735bfd2ee7c0b48c6c29bbf50a
zerontec <zerontecnologia1@gmail.com>
2023-09-06 19:03:54 -0400

    add loanEmployedPages

---
+++ src/pages/LoanEmployePages.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { EmployedLoanAdmin } from '../sections/@dashboard/EmployedLoanAdmin';
+
+
+
+
+const LoanEmployePages
+= ()=>(
+
+    <>
+
+    <EmployedLoanAdmin/>
+    
+    </>
+
+)
+
+
+export default LoanEmployePages
+;
