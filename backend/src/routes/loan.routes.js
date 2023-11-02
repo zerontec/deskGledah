a74ecdc885c5b748e038c526359123e7151210e0
zerontec <zerontecnologia1@gmail.com>
2023-11-02 18:18:15 -0400

    add routes get-payments

--- backend/src/routes/loan.routes.js
+++ backend/src/routes/loan.routes.js
@@ -6,7 +6,7 @@ const authJwt = require('../middleware/authJwt');
 const router = Router();
 
 router.get('/get-all',authJwt.verifyToken, authJwt.isAdmin, getAllLoan);
-router.get('/get/:id',authJwt.verifyToken, authJwt.isAdmin, getLoansBySeller);
+router.post('/get-payments',authJwt.verifyToken, authJwt.isAdmin, getLoansBySeller);
 router.post('/create', authJwt.verifyToken, authJwt.isAdmin,createLoan);
 router.put('/update/:id',authJwt.verifyToken, authJwt.isAdmin, updateLoanStatus);
 router.delete('/delete/:id',authJwt.verifyToken, authJwt.isAdmin, deleteLoan);
--- backend/src/routes/loanCustomer.routes.js
+++ backend/src/routes/loanCustomer.routes.js
@@ -6,13 +6,15 @@ const authJwt = require('../middleware/authJwt');
 const router = Router();
 
 router.get('/get-all-loan',authJwt.verifyToken, authJwt.isAdmin, getAllLoan);
-router.get('/get/:id',authJwt.verifyToken, authJwt.isAdmin, getLoansByCustomer);
+router.post('/get-payments',authJwt.verifyToken, authJwt.isAdmin, getLoansByCustomer);
 router.post('/create', authJwt.verifyToken, authJwt.isAdmin,createLoan);
 router.put('/update/:id',authJwt.verifyToken, authJwt.isAdmin, updateLoanStatus);
 router.delete('/delete/:id',authJwt.verifyToken, authJwt.isAdmin, deleteLoan);
 router.post('/payment',authJwt.verifyToken, authJwt.isAdmin, createPayment)
 router.put('/update-payment/:id',authJwt.verifyToken, authJwt.isAdmin, updatePayment);
 router.get('/get-all-payment',authJwt.verifyToken, authJwt.isAdmin, getCompletedPayments)
+router.get('/get-completed-payment', getCompletedPayments);
+
 // router.delete('/delete/:id', deleteLoan);
 
 
